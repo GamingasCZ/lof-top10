afd5a374327e46cc916eeb76e49a418e9fdcf2c3
GamingasCZ <gamingforyou875@gmail.com>
2022-01-02 16:47:02 +0100

    New role editor added

--- js/helpDialogs.js
+++ js/helpDialogs.js
@@ -16,21 +16,13 @@ $(function () {
             "title": "Event - Lepší seznam",
             "content": `Když necháš tohle <b style="color: tomato">zaškrtlé</b>, tvůj určitě mnohem lepší seznam se zobrazí <b style="color: lime">vedlé mého 2021 seznamu.</b>`
         },
-        "roleName": {
-            "title": jsStr["ROLE_NM_T"][LANG],
-            "content": jsStr["ROLE_NM_D"][LANG]
-        },
-        "roleHasPer": {
-            "title": jsStr["ROLE_HP_T"][LANG],
-            "content": jsStr["ROLE_HP_D"][LANG]
-        },
-        "roleColor": {
-            "title": jsStr["ROLE_CO_T"][LANG],
-            "content": jsStr["ROLE_CO_D"][LANG]
-        },
         "humansHelp": {
             "title": jsStr["HUM_T"][LANG],
             "content": jsStr["HUM_D"][LANG]
+        },
+        "lastRoleDelete": {
+            "title": jsStr["RMROLE_T"][LANG],
+            "content": jsStr["RMROLE_D"][LANG]
         }
     }
 })
