974ec42f8a52882a71cc56c01d8148673c60632f
GamingasCZ <gamingforyou875@gmail.com>
2022-07-03 22:36:37 +0200

    Database now returns only up to 15 lists max

--- php/getLists.php
+++ php/getLists.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+Return codes: 
+0 - Connection error
+1 - Empty request
+2 - Invalid parameters
+3 - No results when searching
+4 - Invalid listID
+*/
+
 // My first time doing php... don't kill me :D
 header("Content-Type: application/json"); // Return as JSON
 require("secrets.php");
@@ -13,28 +22,29 @@ if ($mysqli->connect_errno) {
   exit();
 }
 
-function parseResult($rows, $singleList = false)
-{
+function clamp($current, $min, $max) {
+  return max($min, min($max, $current));
+}
+
+function parseResult($rows, $singleList = false, $maxpage = -1) {
   $ind = 0;
   if (!$singleList) {
+    // No results when searching / No lists to load
+    if (count($rows) == 0) {
+      die("3");
+    }
+    
     foreach ($rows as $row) {
-      if (base64_decode($row["data"], true) == true) {
-        $row["data"] = base64_decode($row["data"]);
-      }
+      // Decode html special characters
       $row["data"] = json_decode(htmlspecialchars_decode($row["data"]));
+      $rows[$ind]["data"] = $row["data"];
 
-      if ($singleList) {
-        $rows["data"] = $row["data"];
-      } else {
-        $rows[$ind]["data"] = $row["data"];
-      }
       $ind += 1;
     }
+    array_push($rows, $maxpage);
+    
   } else {
     // Single list
-    if (base64_decode($rows["data"], true) == true) {
-      $rows["data"] = base64_decode($rows["data"]);
-    }
     $rows["data"] = json_decode(htmlspecialchars_decode($rows["data"]));
   }
 
@@ -46,21 +56,17 @@ if (count($_GET) == 1) {
   if (in_array("id", array_keys($_GET))) {
     // Private lists can't be accessed by their id!
     $result = doRequest($mysqli, "SELECT * FROM `lists` WHERE `hidden` = '0' AND `id` = ?", [$_GET["id"]], "s");
-    if (count($result) == 0) {
-      echo "2";
-    } else {
-      parseResult($result, true);
-    }
+
+    if (count($result) == 0) { die("4"); } // Invalid ID
+      
+    parseResult($result, true);
   } elseif (in_array("pid", array_keys($_GET))) {
     // Private lists
     $result = doRequest($mysqli, "SELECT * FROM `lists` WHERE `hidden`= ?", [$_GET["pid"]], "s");
 
-    if (count($result) == 0) {
-      echo "2";
-    } // When the pID is invalid
-    else {
-      parseResult($result, true);
-    }
+    if (count($result) == 0) { die("4"); } // Invalid ID
+    
+    parseResult($result, true);
   } elseif (in_array("random", array_keys($_GET))) {
     // Picking a random list
     $query = $mysqli->query("SELECT `id` FROM `lists` WHERE `hidden`=0");
@@ -77,14 +83,44 @@ if (count($_GET) == 1) {
     $result = $mysqli->query("SELECT * FROM `lists` WHERE `hidden` = '0' ORDER BY `lists`.`id` DESC LIMIT 3 ");
     parseResult($result->fetch_all(MYSQLI_ASSOC));
   }
-} elseif (count($_GET) > 1) {
-  // multiple parameters don't make sense lol
-  echo "3";
-  exit();
 } else {
-  // Loading all lists
-  $result = $mysqli->query("SELECT * FROM `lists` WHERE `hidden` = '0' ORDER BY `lists`.`id` DESC");
-  parseResult($result->fetch_all(MYSQLI_ASSOC));
+  // --- Loading all lists ---
+
+  // Are values numbers?
+  if (!is_numeric($_GET["page"]) &&
+      !is_numeric($_GET["startID"]) &&
+      !is_numeric($_GET["sort"] &&
+      !is_numeric($_GET["fetchAmount"]))) {
+    die("1");
+  }
+
+  // How many list should be fetched and the offset (page)
+  $dbSlice = clamp(intval($_GET["fetchAmount"]), 2, 15) * intval($_GET["page"]);
+  
+  // 0 = descending, 1 = ascending
+  $sorting = intval($_GET["sort"]) == 0 ? "DESC" : "ASC";
+
+  $query = sprintf("SELECT * FROM `lists`
+            WHERE `hidden` = '0'
+            AND `name` LIKE '%%%s%%'
+            ORDER BY `lists`.`id`
+            %s 
+            LIMIT %d
+            OFFSET %d", $_GET["searchQuery"], $sorting, $_GET["fetchAmount"], $dbSlice);
+
+  $maxpageQuery = $mysqli->query(sprintf("SELECT COUNT(*) from `lists` WHERE `hidden` = '0' AND `name` LIKE '%%%s%%'", $_GET["searchQuery"]));
+  $maxpage = ceil($maxpageQuery->fetch_array()[0] / $_GET["fetchAmount"]);
+  
+  $result = $mysqli->query($query);
+  parseResult($result->fetch_all(MYSQLI_ASSOC), false, $maxpage);
 }
 
 $mysqli->close();
+
+
+/*
+GET
+&id || pid || random || homepage
+
+&page=0?startID=10?searchQuery=hello?sort=0?fetchAmount=10
+*/
